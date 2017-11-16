5250b5041fce96e2113c272ca3ba08cd4bd30f57
youwillneverguesswhoiam <j.mikulska2@student.uw.edu.pl>
2017-11-16 15:44:26 +0100

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,34 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import javax.swing.table.TableColumn;
+import javax.swing.text.TableView;
+import java.awt.*;
+
+public class Controller {
+
+    @FXML public TextField tI;
+    @FXML public TextField tN;
+    @FXML public TextField tWi;
+    @FXML public TextField tWz;
+    @FXML public TextField tP;
+
+    @FXML public Button guzik;
+
+    @FXML public TableView tabelka;
+    @FXML public TableColumn colI;
+    @FXML public TableColumn colN;
+    @FXML public TableColumn colWi;
+    @FXML public TableColumn colWz;
+    @FXML public TableColumn colP;
+    @FXML public TableColumn colU;
+
+    @FXML public void handleClick(ActionEvent actionEvent) {
+
+
+
+    }
+
+}
