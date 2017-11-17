fdf6fc9d111a2f19b9f60778007e21e0cd307d03
radziq1302 <radziq1302@gmail.com>
2017-11-17 16:39:06 +0100

    20171117
    drugi tomka

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,8 +4,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+
 
 import javax.swing.table.TableColumn;
+import javax.xml.soap.Text;
 
 public class Controller {
     public javafx.scene.control.TableView<Człowiek> tabelka;
@@ -16,8 +19,8 @@ public class Controller {
     @FXML public TextField tP;
 
     public Button guzik;
-
-
+    public String dupka_imie;
+    public Człowiek czlek;
     @FXML public TableColumn colI;
     @FXML public TableColumn colN;
     @FXML public TableColumn colWi;
@@ -26,11 +29,15 @@ public class Controller {
     @FXML public TableColumn colU;
 
     @FXML public void handleClick(ActionEvent actionEvent) {
-    tabelka.getItems().add(new Człowiek("tomek", "tomek"));
-
-
-
+    tabelka.getItems().add(czlek = new Człowiek(tI.getText(), "tomek"));
+    dupka_imie = czlek.getImię();
 
     }
-
+    public void initialize() {
+        for (javafx.scene.control.TableColumn<Człowiek, ?> column : tabelka.getColumns())
+         {
+             javafx.scene.control.TableColumn<Człowiek, String> textColumn = (javafx.scene.control.TableColumn<Człowiek, String>) column;
+             textColumn.setCellValueFactory(new PropertyValueFactory<>("dupka_imie"));
+        }
+        }
 }
